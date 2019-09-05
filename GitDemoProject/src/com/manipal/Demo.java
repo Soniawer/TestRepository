3b319577e046b81e5fc2e0e121a28f7dcd6bef4f
ITFS <ITFS@ITFS-17>
2019-09-05 11:26:49 +0530

    updated second line

--- GitDemoProject/src/com/manipal/Demo.java
+++ GitDemoProject/src/com/manipal/Demo.java
@@ -4,7 +4,8 @@ public class Demo {
 
 	public static void main(String[] args)
 	{
-	System.out.println("hello Git");
+	System.out.println("hello Git ...");
+	System.out.println("second line is added");
 	}
 
 }
